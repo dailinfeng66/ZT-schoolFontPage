013cadf77aeafcc698931b94db17637c77eb30e8
dailinfeng <dailinfeng66@163.com>
2020-02-22 14:58:18 +0800

    fix

--- src/api/xy_lostAndFound.js
+++ src/api/xy_lostAndFound.js
@@ -1,82 +1,79 @@
 import request from '@/utils/request'
 // 从这儿开始
 export function findAll(data1) {
-  return request({
-    url: '/lost/mgr/lost/list/'+data1.page+"/"+data1.size+"/"+data1.title,
-    method: 'post',
-    data: data1.mgrQueryLostRequest
-  })
+    return request({
+        url: '/lost/mgr/lost/list/' + data1.page + "/" + data1.size + "/" + data1.title,
+        method: 'post',
+        data: data1.mgrQueryLostRequest
+    })
 }
 export function getInfo(id) {
-  return request({
-    url: 'lost/mgr/lost/'+id,
-    method: 'get',
-    // params: {
-    //   roleId
-    // }
-  })
+    return request({
+        url: 'lost/mgr/lost/' + id,
+        method: 'get',
+        // params: {
+        //   roleId
+        // }
+    })
 }
-
-
-
 export function getRole(roleId) {
-  return request({
-    url: '/role/getRole',
-    method: 'get',
-    params: {
-      roleId
-    }
-  })
+    return request({
+        url: '/role/getRole',
+        method: 'get',
+        params: {
+            roleId
+        }
+    })
 }
 
 export function createRole(roleInfo) {
-  return request({
-    url: '/role/createRole',
-    method: 'post',
-    data: roleInfo
-  })
+    return request({
+        url: '/role/createRole',
+        method: 'post',
+        data: roleInfo
+    })
 }
 export function updateRole(roleInfo) {
-  return request({
-    url: '/role/updateRole',
-    method: 'post',
-    data: roleInfo
-  })
+    return request({
+        url: '/role/updateRole',
+        method: 'post',
+        data: roleInfo
+    })
 }
 export function deleteRole(ids) {
-  return request({
-    url: '/role/deleteRole',
-    method: 'get',
-    params: {
-      ids
-    }
-  })
+    return request({
+        url: '/role/deleteRole',
+        method: 'get',
+        params: {
+            ids
+        }
+    })
 }
 export function findMenuTree(pv) {
-  return request({
-    url: '/menu/findMenuTree',
-    method: 'get',
-    params: {
-      pv
-    }
-  })
+    return request({
+        url: '/menu/findMenuTree',
+        method: 'get',
+        params: {
+            pv
+        }
+    })
 }
 export function getMenu(menuId) {
-  return request({
-    url: '/menu/getMenu',
-    method: 'get',
-    params: {
-      menuId
-    }
-  })
+    return request({
+        url: '/menu/getMenu',
+        method: 'get',
+        params: {
+            menuId
+        }
+    })
 }
 export function updateJurisdiction(ids, roleId) {
-  return request({
-    url: '/role/updateJurisdiction',
-    method: 'get',
-    params: {
-      ids,
-      roleId
-    }
-  })
+    return request({
+        url: '/role/updateJurisdiction',
+        method: 'get',
+        params: {
+            ids,
+            roleId
+        }
+    })
 }
