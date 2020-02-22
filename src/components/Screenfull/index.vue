9a1283a33059de74c0723a10b45e90a8fff86e92
dailinfeng <dailinfeng66@163.com>
2020-02-22 14:53:17 +0800

    fix

--- src/components/Screenfull/index.vue
+++ src/components/Screenfull/index.vue
@@ -1,63 +1,74 @@
 <template>
-<div>
-    <el-input placeholder="学校名（ID）" style="width: 200px;" />
-    <svg-icon :icon-class="isFullscreen?'exit-fullscreen':'fullscreen'" @click="click" />
-</div>
+  <div>
+    <el-select v-model="value" disabled style="width: 200px;">
+      <el-option :label="item.label" :value="item.value"> </el-option>
+    </el-select>
+    <svg-icon
+      :icon-class="isFullscreen ? 'exit-fullscreen' : 'fullscreen'"
+      @click="click"
+    />
+  </div>
 </template>
 
 <script>
-import screenfull from 'screenfull'
-
-export default{
-name: 'Screenfull',
-    data() {
-        return {
-            isFullscreen: false
-        }
+import screenfull from "screenfull";
+import { getSchoolId } from "@/utils/auth";
+export default {
+  name: "Screenfull",
+  data() {
+    return {
+      isFullscreen: false,
+      value: null,
+      item: null
+    };
+  },
+  mounted() {
+    this.init();
+  },
+  beforeDestroy() {
+    this.destroy();
+  },
+  methods: {
+    click() {
+      if (!screenfull.enabled) {
+        this.$message({
+          message: "you browser can not work",
+          type: "warning"
+        });
+        return false;
+      }
+      screenfull.toggle();
     },
-    mounted() {
-        this.init()
+    change() {
+      this.isFullscreen = screenfull.isFullscreen;
     },
-    beforeDestroy() {
-        this.destroy()
+    init() {
+     let schoolId = getSchoolId()
+      this.item = {
+        label: schoolId,
+        value: schoolId
+      };
+      this.value = schoolId;
+      if (screenfull.enabled) {
+        screenfull.on("change", this.change);
+      }
     },
-    methods: {
-        click() {
-            if (!screenfull.enabled) {
-                this.$message({
-                    message: 'you browser can not work',
-                    type: 'warning'
-                })
-                return false
-            }
-            screenfull.toggle()
-        },
-        change() {
-            this.isFullscreen = screenfull.isFullscreen
-        },
-        init() {
-            if (screenfull.enabled) {
-                screenfull.on('change', this.change)
-
-            }
-        },
-        destroy() {
-            if (screenfull.enabled) {
-                screenfull.off('change', this.change)
-            }
-        }
+    destroy() {
+      if (screenfull.enabled) {
+        screenfull.off("change", this.change);
+      }
     }
-}
+  }
+};
 </script>
 
 <style scoped>
 .screenfull-svg {
-    display: inline-block;
-    cursor: pointer;
-    fill: #5a5e66;
-    ;
-    width: 20px;
-    height: 20px;
-    vertical-align: 10px;
+  display: inline-block;
+  cursor: pointer;
+  fill: #5a5e66;
+  width: 20px;
+  height: 20px;
+  vertical-align: 10px;
 }
 </style>
